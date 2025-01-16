bc4bcf179945da726cda6a472bb1150d3204b8ed
Mr kin <kiengithub@gmail.com>
2025-01-16 13:55:14 +0700

    nop bai lan 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
         // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+        int myInt = 42;
+        double myDouble = 3.14D;
+        String myString = "Hello, Java!";
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myString);
+
+        if (myInt > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Less or equal to 10");
+        }
     }
 }
